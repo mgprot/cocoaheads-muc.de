4eacce289449469c64abe0519d6a5dd95a7f4d4d
Marcus Rohrmoser <mrohrmoser@acm.org>
2014-02-10 18:47:33 +0100

    README++, detab.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Source of <http://cocoaheads-muc.de/>
 
-## Prepare a talk
+## Prepare a talk (jumpstart)
 
 1. fork repository,
-2. `git clone git@github.com:mro/cocoaheads-muc.de.git && cd cocoaheads-muc.de`
-3. install nanoc: `$ sudo gem install nanoc kramdown`
+2. clone repository: `$ git clone git@github.com:mro/cocoaheads-muc.de.git && cd cocoaheads-muc.de`
+3. install [nanoc](http://nanoc.ws/): `$ sudo gem install nanoc kramdown`
 4. add a post: `$ mkdir content/posts/<iso_date> && vi content/posts/<iso_date>/index.md`
 5. hack, hack, hack
 6. `$ nanoc compile && nanoc view`
--- lib/default.rb
+++ lib/default.rb
@@ -17,51 +17,58 @@ I18n.default_locale = :de
 
 
 class String
+  # Render a localised, human readable date+time
+  #
   # http://www.ruby-doc.org/stdlib-1.8.7/libdoc/date/rdoc/DateTime.html
   def to_date_time style
-  	t = DateTime.parse(self)
-  	case style
-  	when :human: I18n.localize t, :format => :long
-  	else raise "Unknown style: '#{style}'"
-  	end
+    t = DateTime.parse(self)
+    case style
+    when :human: I18n.localize t, :format => :long
+    else raise "Unknown style: '#{style}'"
+    end
   end
 end
 
 
+# Add a bit of magic.
 #
-# https://stackoverflow.com/questions/4470108/when-monkey-patching-a-method-can-you-call-the-overridden-method-from-the-new-i
 class Nanoc::Item
   # Monkey-patch [] operator to automatically pick up post date from identifier
-	alias_method :old_acc, :[]
+  # https://stackoverflow.com/questions/4470108/when-monkey-patching-a-method-can-you-call-the-overridden-method-from-the-new-i
+  alias_method :old_acc, :[]
 
-	def [](key)
-		ret = old_acc(key)
-		(ret.nil? && key == :date) ? date_from_post_path : ret
-	end
+  def [](key)
+    ret = old_acc(key)
+    (ret.nil? && key == :date) ? date_from_post_path : ret
+  end
 
-	# 
-	def date_from_post_path
-		m = REGEXP_DATE_ISO.match(self.identifier)
-		time = m[2].nil? ? '19:00:00' : m[2..4].join(':')
-		return m.nil? ? nil : [m[1], time].join('T')
-	end
+  # Extract date+time from identifier.
+  # 
+  # default time to 19:00:00 if not part of the identifier.
+  def date_from_post_path
+    m = REGEXP_DATE_ISO.match(self.identifier)
+    time = m[2].nil? ? '19:00:00' : m[2..4].join(':')
+    return m.nil? ? nil : [m[1], time].join('T')
+  end
 
-	def title_folded_to_filename
-		self[:title].gsub(/[^a-z0-9-]/) do |c|
-			case c
-			when /\s+|\./: '-'
-			when /[A-Z]+/: c.downcase
-			else ''
-			end
-		end.gsub(/\-+/,'-')
-	end
+  # Turn :title into something sensible as URL.
+  def title_folded_to_filename
+    self[:title].gsub(/[^a-z0-9-]/) do |c|
+      case c
+      when /\s+|\./: '-'
+      when /[A-Z]+/: c.downcase
+      else ''
+      end
+    end.gsub(/\-+/,'-')
+  end
 end
 
 
+# Return array with all posts sorted by date (descending)
 def sorted_posts_by_date
   @items.select do |i|
-  	! ( REGEXP_DATE_ISO.match(i.identifier).nil? )
+    ! ( REGEXP_DATE_ISO.match(i.identifier).nil? )
   end.sort do |a,b|
-  	a.identifier <=> b.identifier
+    a.identifier <=> b.identifier
   end.reverse
 end
